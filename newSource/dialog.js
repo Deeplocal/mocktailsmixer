aed18c1e7d00199eb3c2a3ecc96f9ed5e8e2cac9
picpicpicpicnic <sscanlan@deeplocal.com>
2023-07-19 11:43:18 -0400

    Cleanup, making classes

---
+++ newSource/dialog.js
@@ -0,0 +1,10 @@
+const dialogflow = require('@google-cloud/dialogflow');
+const { v4: uuidv4 } = require('uuid');
+
+class DialogComponent {
+  constructor() {
+    // WTF goes here???
+  }
+}
+
+module.exports = { DialogComponent };
