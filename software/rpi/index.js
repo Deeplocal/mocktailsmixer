3a139d5ca8c39f2b784025740ca6b21e382a8f86
Imani Emanuel <iemanuel@deeplocal.com>
2023-08-25 11:56:16 -0400

    really changed mechanical from b1 to b5

--- software/rpi/index.js
+++ software/rpi/index.js
@@ -163,14 +163,14 @@ function keyWordToArduino(keyword) {
   else if (keyword.includes('mechanical')) {
     //open b1 lemonade after 1 second
     setTimeout(() => {
-      port.write("b1r!"); console.log("sending:", "b2r!");
+      port.write("b2r!"); console.log("sending:", "b2r!");
       //open b5 watermelon after 2 seconds
       setTimeout(() => {
         port.write("b5r!"); console.log("sending:", "b5r!");
       }, 2000)
       // after 44 seconds close b1 lemonade
       setTimeout(() => {
-        port.write("b1l!"); console.log("sending:", "b2l!")
+        port.write("b2l!"); console.log("sending:", "b2l!")
         //after one second of b1 close, close b5 watermelon
         setTimeout(() => {
           port.write("b5l!"); console.log("sending:", "b5l!")
